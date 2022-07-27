add1e07fab8dbe77dcb6d705b46b4430eaf82774
GregoryLi10 <90701941+GregoryLi10@users.noreply.github.com>
2022-07-27 05:34:25 -0700

    finally finished

--- MapDisplay.java
+++ MapDisplay.java
@@ -1,8 +1,10 @@
+import java.awt.AWTException;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.MouseInfo;
+import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -29,7 +31,7 @@ public class MapDisplay {
 	
 	private int ix=0, iy=0, mx=0, my=0;
 	private int zoom=0, zoomDiv=10;
-	private final int zoomMin=-zoomDiv/2, zoomMax=zoomDiv*3;
+	private final int zoomMin=-zoomDiv/2, zoomMax=zoomDiv*5;
 	
 	private int[] screensize={(int) Toolkit.getDefaultToolkit().getScreenSize().getWidth(), (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight()};
 	
@@ -45,15 +47,20 @@ public class MapDisplay {
 		
 		JPanel canvas = new JPanel() {
 			public void paint(Graphics g) {
-				if (img==null) return;
-				if (ix>=width)
-					ix-=width;
-				else if (ix+width+zoom*width/zoomDiv<=0)
-					ix+=width;
-				if (iy>=height)
-					iy-=height;
-				else if (iy+height+zoom*height/zoomDiv<=0)
-					iy+=height;
+				if (img==null) return; //if image has not loaded yet, don't draw
+				
+				//bring image back to screen if offscreen
+//				if (ix>=width)
+//					ix-=width+zoom*width/zoomDiv;
+//				else if (ix+width+zoom*width/zoomDiv<=0)
+//					ix+=width+zoom*width/zoomDiv;
+//				
+//				if (iy>=height)
+//					iy-=height+zoom*height/zoomDiv;
+//				else if (iy+height+zoom*height/zoomDiv<=0)
+//					iy+=height+zoom*height/zoomDiv;
+				
+				//when in navigation mode, move image with cursor
 				if (navigating) {
 					int mxNew=MouseInfo.getPointerInfo().getLocation().x, myNew=MouseInfo.getPointerInfo().getLocation().y;
 					ix+=mxNew-mx;
@@ -61,14 +68,47 @@ public class MapDisplay {
 					mx=mxNew;
 					my=myNew;
 				}
+				
+				//switch main image to largest on screen
+				if (ix-(width+zoom*width/zoomDiv)>0&&ix+width+zoom*width/zoomDiv>width) 
+					ix=ix-(width+zoom*width/zoomDiv);
+				else if (ix+width+zoom*width/zoomDiv<width&&ix<0) 
+					ix=ix+width+zoom*width/zoomDiv;
+				
+				if (iy-(height+zoom*height/zoomDiv)>0&&iy+height+zoom*height/zoomDiv>height) 
+					iy=iy-(height+zoom*height/zoomDiv);
+				else if (iy+height+zoom*height/zoomDiv<height&&iy<0) 
+					iy=iy+height+zoom*height/zoomDiv;
+				
+				//draw background and main map
 				g.setColor(new Color(50,50,50));
 				g.fillRect(0, 0, screensize[0], screensize[1]);
 				g.drawImage(img,ix,iy,width+zoom*width/zoomDiv,height+zoom*height/zoomDiv,this);
+				
+				//draw side and corner maps if screen is not filled by main map
+				if (ix>0) {
+					g.drawImage(img,ix-(width+zoom*width/zoomDiv),iy,width+zoom*width/zoomDiv,height+zoom*height/zoomDiv,this);
+					if (iy>0) 
+						g.drawImage(img,ix-(width+zoom*width/zoomDiv),iy-(height+zoom*height/zoomDiv),width+zoom*width/zoomDiv,height+zoom*height/zoomDiv,this);
+					if (iy+height+zoom*height/zoomDiv<height) 
+						g.drawImage(img,ix-(width+zoom*width/zoomDiv),iy+height+zoom*height/zoomDiv,width+zoom*width/zoomDiv,height+zoom*height/zoomDiv,this);
+				}
+				if (ix+width+zoom*width/zoomDiv<width) {
+					g.drawImage(img,ix+width+zoom*width/zoomDiv,iy,width+zoom*width/zoomDiv,height+zoom*height/zoomDiv,this);
+					if (iy>0) 
+						g.drawImage(img,ix+width+zoom*width/zoomDiv,iy-(height+zoom*height/zoomDiv),width+zoom*width/zoomDiv,height+zoom*height/zoomDiv,this);
+					if (iy+height+zoom*height/zoomDiv<height) 
+						g.drawImage(img,ix+width+zoom*width/zoomDiv,iy+height+zoom*height/zoomDiv,width+zoom*width/zoomDiv,height+zoom*height/zoomDiv,this);
+				}
+				if (iy>0) 
+					g.drawImage(img,ix,iy-(height+zoom*height/zoomDiv),width+zoom*width/zoomDiv,height+zoom*height/zoomDiv,this);
+				if (iy+height+zoom*height/zoomDiv<height) 
+					g.drawImage(img,ix,iy+height+zoom*height/zoomDiv,width+zoom*width/zoomDiv,height+zoom*height/zoomDiv,this);
 			}
 		};
 		canvas.addMouseListener(new MouseListener() {
 			@Override
-			public void mousePressed(MouseEvent e) {
+			public void mousePressed(MouseEvent e) { //activate navigation mode
 				navigating=true;
 				mx=MouseInfo.getPointerInfo().getLocation().x;
 				my=MouseInfo.getPointerInfo().getLocation().y;
@@ -78,7 +118,7 @@ public class MapDisplay {
 			public void mouseClicked(MouseEvent e) {}
 
 			@Override
-			public void mouseReleased(MouseEvent e) {
+			public void mouseReleased(MouseEvent e) { //stop navigation mode
 				navigating=false;
 			}
 
@@ -92,14 +132,14 @@ public class MapDisplay {
 		canvas.addMouseWheelListener(new MouseWheelListener(){
 
 			@Override
-			public void mouseWheelMoved(MouseWheelEvent e) {
+			public void mouseWheelMoved(MouseWheelEvent e) { //zooms in on mouse cursor location
 				if (!e.isControlDown()) { // scrolling
 					if (zoom<=zoomMin&&e.getWheelRotation()<0)
 						return;
 					else if (zoom>=zoomMax&&e.getWheelRotation()>0) 
 						return;
 					
-					int ow=width+zoom*width/zoomDiv, oh=height+zoom*height/zoomDiv;
+					int ow=width+zoom*width/zoomDiv, oh=height+zoom*height/zoomDiv; //old image dimensions
 					
 					zoom+=e.getWheelRotation(); // negative = down, positive = up
 					if (zoom<zoomMin)
@@ -107,9 +147,10 @@ public class MapDisplay {
 					else if (zoom>zoomMax) 
 						zoom=zoomMax;
 					
-					int nw=width+zoom*width/zoomDiv, nh=height+zoom*height/zoomDiv;
+					int nw=width+zoom*width/zoomDiv, nh=height+zoom*height/zoomDiv; //new image dimensions
 					
-					ix-=(ix+nw-e.getX())-(ix+ow-e.getX())*(nw/(double)ow);
+					//calculate new image coords relative to cursor location and zoom
+					ix-=(ix+nw-e.getX())-(ix+ow-e.getX())*(nw/(double)ow); 
 					iy-=(iy+nh-e.getY())-(iy+oh-e.getY())*(nh/(double)oh);
 				} 
 			}
@@ -122,7 +163,14 @@ public class MapDisplay {
 			public void keyTyped(KeyEvent e) {}
 
 			@Override
-			public void keyPressed(KeyEvent e) {}
+			public void keyPressed(KeyEvent e) { //center mouse
+				try {
+					Robot robot = new Robot();
+					robot.mouseMove(width/2, height/2);
+				} catch (AWTException e1) {
+					e1.printStackTrace();
+				}
+			}
 
 			@Override
 			public void keyReleased(KeyEvent e) {}
@@ -138,7 +186,6 @@ public class MapDisplay {
 		try {
 			Thread.sleep(250);
 		} catch (InterruptedException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 		frame.getContentPane().repaint();
@@ -146,10 +193,9 @@ public class MapDisplay {
 			try {
 				Thread.sleep(15);
 			} catch (InterruptedException e1) {
-				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
-			frame.getContentPane().repaint();
+			frame.getContentPane().repaint(); //new frame every 15ms
 		}
 	}
 	
